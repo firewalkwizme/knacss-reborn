635ded1d9185a941ab14422d0e606908dc651daa
Raphaël Goetter <raphael@goetter.fr>
2020-10-21 13:41:15 +0200

    gulpfile fonctionnel

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
 const gulp = require('gulp');
+var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var CombineMQ = require('postcss-combine-media-query');
-var CSSnano = require('cssnano');
 var autoprefixer = require('autoprefixer');
+var CSSnano = require('cssnano');
+var rename = require('gulp-rename');
 
-gulp.task('css', function () {
+gulp.task('css:full', () => {
   return gulp.src('sass/knacss.scss')
+    .pipe(sass(
+      {
+      outputStyle: 'expanded'
+      }))
+    .pipe(
+      postcss([
+        // CombineMQ,
+        autoprefixer,
+        // CSSnano
+      ]))
+    .pipe(gulp.dest('css/knacss-full'));
+});
+
+gulp.task('css:mini', () => {
+  return gulp.src('sass/knacss.scss')
+    .pipe(sass(
+      {
+      outputStyle: 'compact'
+      }))
     .pipe(
       postcss([
         CombineMQ,
         autoprefixer,
-        // CSSnano
+        CSSnano
       ]))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('css/knacss-mini'));
 });
 
-gulp.task('default', ['css']);
+// Tâche BUILD : tapez "gulp" ou "gulp build"
+gulp.task('build', gulp.series('css:full', 'css:mini'));
+
+// Tâche par défaut
+gulp.task('default', gulp.series('build'));
